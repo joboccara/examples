1b64f58da20eb6eacdf111fa31a87d2adde09c20
JoBoccara <joboc100@hotmail.com>
2017-02-19 00:06:10 +0100

    Add merge_request source file

---
+++ merge_adjacent/merge_adjacent.cpp
@@ -0,0 +1,43 @@
+#include <algorithm>
+#include <iostream>
+#include <iterator>
+#include <vector>
+
+template <typename ForwardIterator, typename OutputIterator>
+OutputIterator merge_adjacent(ForwardIterator first, ForwardIterator last, OutputIterator results)
+{
+    ForwardIterator beginUnique = first;
+    while (beginUnique != last)
+    {     
+      // output all unique elements; stop when finding indentical adjacent ones
+      ForwardIterator endUnique = std::adjacent_find(beginUnique, last);
+      std::copy(beginUnique, endUnique, results);
+    
+      // identify the range of identical adjacent elements
+      ForwardIterator beginIdentical = endUnique;
+      ForwardIterator endIdentical = std::find_if(beginIdentical, last, [beginIdentical](const auto& element) {return element != *beginIdentical;});
+    
+      // aggregate identical flows into one
+      if (beginIdentical != endIdentical)
+      {
+         *results = std::accumulate(beginIdentical, endIdentical, typename ForwardIterator::value_type());
+         ++results;
+      }
+      beginUnique = endIdentical;
+    }
+    return results;
+}
+
+int main()
+{
+    std::vector<int> v = { 1, 1, 1, 4, 3, 3, 10, -5 };
+    std::vector<int> results;
+
+    merge_adjacent(v.begin(), v.end(), std::back_inserter(results));
+
+    std::copy(results.begin(), results.end(), std::ostream_iterator<int>(std::cout, " "));
+    std::cout << '\n';
+
+    return 0;
+}
+
